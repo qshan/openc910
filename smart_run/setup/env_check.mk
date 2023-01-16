c03fb259b9965e109fc8d252d0c20664bb017607
Frank Shan <frankqs0304@hotmail.com>
2023-01-17 01:09:30 +0800

    update format

--- smart_run/setup/env_check.mk
+++ smart_run/setup/env_check.mk
@@ -15,16 +15,16 @@
 
 tool-chain-chk:
 ifndef TOOL_EXTENSION
-	$(warning \
-        $(newline)   The environment varialble $$TOOL_EXTENSION must be set to specify the GNU \
-        $(newline)   toolchains to compile .c/.s smart test cases. \
-        $(newline)   $$TOOL_EXTENSION must be set as shown in ./setup/example_setup.csh, and the \
-        $(newline)   suggested steps are:  \
-        $(newline)       1. vim ./setup/example_setup.csh \
-        $(newline)       2. Modify the example_setup.csh with the real toolchain path according \
-        $(newline)          to your environment. \
-        $(newline)       3. source ./setup/example_setup.csh )
+#	if ("" == $TOOL_EXTENSION) then
+		$(warning \
+				$(newline)   The environment varialble $$TOOL_EXTENSION must be set to specify the GNU \
+				$(newline)   toolchains to compile .c/.s smart test cases. \
+				$(newline)   $$TOOL_EXTENSION must be set as shown in ./setup/example_setup.csh, and the \
+				$(newline)   suggested steps are:  \
+				$(newline)       1. vim ./setup/example_setup.csh \
+				$(newline)       2. Modify the example_setup.csh with the real toolchain path according \
+				$(newline)          to your environment. \
+				$(newline)       3. source ./setup/example_setup.csh )
 else
-	@echo "  Toolchain path: ${TOOL_EXTENSION}"
+		@echo "  Toolchain path: ${TOOL_EXTENSION}"
 endif
-
